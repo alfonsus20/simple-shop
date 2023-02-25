d6e90c523a26a33b10202a4ff6d8701c353075b3
alfonsus20 <alfonschandrawan@gmail.com>
2023-02-26 00:34:47 +0800

    add logo and font

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,18 @@
-import { Box, Circle, Flex, IconButton } from "@chakra-ui/react";
+import { Box, Circle, Flex, IconButton, Text } from "@chakra-ui/react";
+import Image from "next/image";
 import Link from "next/link";
-
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useCartContext } from "src/context/CartContext";
+import Logo from "../../public/logo.png";
 
 const Header = () => {
   const { cartItems } = useCartContext();
 
   return (
-    <Box bg="gray.700" position="sticky" top={0} zIndex={10}>
+    <Box bg="gray.700" shadow="md" position="sticky" top={0} zIndex={10}>
       <Flex
         px={10}
-        py={{ base: 4, md: 6 }}
+        py={{ base: 4 }}
         alignItems="center"
         color="white"
         maxW="container.xl"
@@ -19,10 +20,18 @@ const Header = () => {
         columnGap={4}
         justifyContent="space-between"
       >
-        <Box as={Link} href="/">
-          SIMPLE SHOP
-        </Box>
-
+        <Flex
+          alignItems="center"
+          flexDir="column"
+          textAlign="center"
+          as={Link}
+          href="/"
+        >
+          <Box position="relative" w={20} style={{ aspectRatio: 4 / 3 }}>
+            <Image src={Logo} alt="logo" objectFit="contain" layout="fill" />
+          </Box>
+          <Text fontWeight="semibold">Simple Shop</Text>
+        </Flex>
         <Link href="/cart">
           <Box pos="relative">
             <IconButton
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import CartProvider from "src/context/CartContext";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: ["400", "500", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
 
 const queryClient = new QueryClient();
 
@@ -10,7 +17,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <ChakraProvider>
       <CartProvider>
         <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
+          <main className={roboto.className}>
+            <Component {...pageProps} />
+          </main>
         </QueryClientProvider>
       </CartProvider>
     </ChakraProvider>
